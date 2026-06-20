03c112d11a2b31fd390d4eab81c945e929212807
Samar49 <samshkr025@gmail.com>
2026-06-20 10:17:57 +0530

    added the function to load employees from files

--- Employee Management System/EmployeeManager.cpp
+++ Employee Management System/EmployeeManager.cpp
@@ -11,8 +11,8 @@ EmployeeManager::EmployeeManager(){
 }
 // Check duplicate ID
 bool EmployeeManager::isDuplicateID(int id) const{
-    for (const Employee &employee:employees){
-        if (employee.getEmployeeID()==id){
+    for(const Employee &employee:employees){
+        if(employee.getEmployeeID()==id){
             return true;
         }
     }
@@ -21,14 +21,36 @@ bool EmployeeManager::isDuplicateID(int id) const{
 // Save all employees to file
 void EmployeeManager::saveToFile() const{
     ofstream file("employees.txt");
-    if (!file){
+    if(!file){
         cout<<"Error opening file.\n";
         return;
     }
-    for (const Employee &employee : employees){
+    for(const Employee &employee:employees){
         file<<employee.toFileString()<<endl;
     }
     file.close();
 }
 // Load employees from file
-void EmployeeManager::loadFromFile()
+void EmployeeManager::loadFromFile(){
+    ifstream file("employees.txt");
+    if(!file){
+        return;
+    }
+    employees.clear();
+    string line;
+    while (getline(file, line)){
+        stringstream ss(line);
+        string idStr, name, ageStr, department, salaryStr;
+        getline(ss, idStr, ',');
+        getline(ss, name, ',');
+        getline(ss, ageStr, ',');
+        getline(ss, department, ',');
+        getline(ss, salaryStr, ',');
+        int id = stoi(idStr);
+        int age = stoi(ageStr);
+        double salary = stod(salaryStr);
+        Employee employee(id, name, age, department, salary);
+        employees.push_back(employee);
+    }
+    file.close();
+}
