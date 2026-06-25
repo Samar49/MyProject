fed127693fc5c880be6efae8b418548c191dc501
Samar49 <samshkr025@gmail.com>
2026-06-25 14:15:42 +0530

    Main file creation

---
+++ Employee Management System/main.cpp
@@ -0,0 +1,48 @@
+#include "EmployeeManager.h"
+using namespace std;
+void showMenu(){
+    cout<<"\n========== Employee Management System ==========\n";
+    cout<<"1. Add Employee\n";
+    cout<<"2. Display All Employees\n";
+    cout<<"3. Search Employee by ID\n";
+    cou<<"4. Update Employee\n";
+    cout<<"5. Delete Employee\n";
+    cout<<"6. Exit\n";
+    cout<<"Enter your choice: ";
+}
+int main(){
+    EmployeeManager manager;
+    int choice;
+    do{
+        showMenu();
+        while(!(cin>>choice)){
+            cout<<"Invalid input. Enter number only: ";
+            cin.clear();
+            cin.ignore(1000, '\n');
+        }
+        switch (choice)
+        {
+        case 1:
+            manager.addEmployee();
+            break;
+        case 2:
+            manager.displayAllEmployees();
+            break;
+        case 3:
+            manager.searchEmployeeByID();
+            break;
+        case 4:
+            manager.updateEmployee();
+            break
+        case 5:
+            manager.deleteEmployee();
+            break;
+        case 6:
+            cout<<"Exiting Program...\n";
+            break;
+        default:
+            cout<<"Invalid choice.\n";
+        }
+    }while (choice!= 6);
+    return 0;
+}
